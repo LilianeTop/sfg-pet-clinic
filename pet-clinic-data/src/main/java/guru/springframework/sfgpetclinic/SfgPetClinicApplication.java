e02ebbdb7a0ba2176d1140dee7b0f9255aef3cf7
LilianeTop <hallo@lilianetop.nl>
2021-05-14 16:57:59 +0200

    solved @SpringBootTest(classes = Application.class),where the Application.class is the @SpringBootApplication class.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -10,7 +10,6 @@ public class SfgPetClinicApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(SfgPetClinicApplication.class, args);
-        System.out.println("Application started");
     }
 
 }
